ab9197602307d2b6931f2c20f2e53b0d78aba383
Tim1380 <98985575+TimDementiev@users.noreply.github.com>
2022-09-12 01:23:48 +0300

    init commit

--- scripts/slide-by-radio.js
+++
@@ -1,9 +0,0 @@
-const sideBar = document.querySelector(".container_type_bicycles"),
-  selector = document.querySelectorAll('input[name="radio-bicycles"]');
-
-for (let i = 0; i < selector.length; i++) {
-  selector[i].addEventListener("change", function () {
-    let val = this.value;
-    sideBar.style.transform = `translateX(-${val * 102.5 - 102.5}%)`;
-  });
-}
